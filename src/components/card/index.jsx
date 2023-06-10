6bb3202cb31c5a7a73a84cc2f6d81620f6f582cb
MaiyaSavinova <hello251127@yandex.ru>
2023-06-10 10:37:42 +0300

    Создание шаблона

---
+++ src/components/card/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.css";
+
+const Card = () => {
+    return <div className="card"/>
+     
+}
+
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
-function Home () {
-   return<>
-   <h1>Главная страница</h1>
+import Layout from "../components/Layout";
+
+function Home() {
+   return <>
+      <h1>Главная страница</h1>
+      <Layout mb={3} dt={1} gap="small" />
+      <Layout mb={4} dt={4} gap="none" />
+      <Layout mb={2} dt={3} />
    </>
 }
-export {Home};
+export { Home };
