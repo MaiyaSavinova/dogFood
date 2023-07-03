66f9715037550d7e2e19b8efb659cd0c78de8462
MaiyaSavinova <hello251127@yandex.ru>
2023-07-04 01:18:15 +0300

    добавление отзыва

--- src/components/Product/Reviews.jsx
+++ src/components/Product/Reviews.jsx
@@ -1,14 +1,20 @@
 const Reviews = ({
     reviews
 }) => {
- 
+
     console.log(reviews);
     return <div className="product__reviews">
         <h2 className="product__title">Отзывы</h2>
         {reviews.map((el, i) => <div key={i}>
-            {el.rating} {el.text}
+        <h4>{el.author.name}</h4>
+            <p className="reviews__text">{el.text}</p>
+            <hr/>
         </div>)}
+      
     </div>
 }
 
 export default Reviews;
+
+
+
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,19 +1,19 @@
-import {useContext, useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 import Layout from "../components/Layout";
 import Product from "../components/Product";
 
 import MainCtx from "../context/main";
 import UtilsCtx from "../context/utils";
-import Main from "../context/main";
 
-function SingleProduct () {
-    const {id} = useParams();
+
+function SingleProduct() {
+    const { id } = useParams();
     const [count, setCount] = useState(0);
-    const {api} = useContext(MainCtx);
-    const {} = useContext(UtilsCtx);
-    const { userId, token } = useContext(Main);
+    const { api } = useContext(MainCtx);
+    const { } = useContext(UtilsCtx);
+    const { userId, token } = useContext(MainCtx);
     const [product, setProduct] = useState({});
     const [text, setText] = useState("");
     const [revActive, setRevActive] = useState(false);
@@ -81,59 +81,46 @@ function SingleProduct () {
     }
 
     return <>
-       {product.name && <Layout title={product.name} dt={2}>
+        {product.name && <Layout title={product.name} dt={2}>
             <div>
-            {product.tags.length > 0 && <Product.Tags tags={product.tags}/>}
+                {product.tags.length > 0 && <Product.Tags tags={product.tags} />}
                 <Product.Image
                     pictures={product.pictures}
                     name={product.name}
-                /> 
+                />
             </div>
-            <Product.Info {...product} setProduct={setProduct}/>
-            <Product.Description description={product.description}/>
-            <Product.Reviews reviews={product.reviews}/>
-            
-      <div className="wrapper__product maxwidth">             
-                    <div className="product__block_left">
-                        <h3>Отзывы</h3>
-                        <button className="product__add-reviews font__bold transition"
-                            onClick={() => setRevActive(true)}>Написать отзыв</button>
-                            
-                        {product.reviews.map((el, i) =>
-                                <div className="product__block_left" key={i}>
-                                    <h4>{el.author.name}</h4>
-                                    <p>{el.text}</p>
-                                    {}
-                                    <hr />
-                                </div>
-                            )}
-                    </div>
-                </div>
+            <Product.Info {...product} setProduct={setProduct} />
+            <Product.Description description={product.description} />
+            <Product.Reviews reviews={product.reviews} />
+            <div className="product__add-reviews">
+                <button className="product__btn"
+                    onClick={() => setRevActive(true)}>Написать отзыв</button>
+                <div>
+                    <div className="product__reviews-text"
+                        style={{ display: revActive ? "flex" : "none" }}>
+                        <div className="reviews">
+                             <button className="btn_close transition"
+                                onClick={() => setRevActive(false)}>х</button>
 
-            <div className="modal__reviews"
-                style={{ display: revActive ? "flex" : "none" }}>
-                <div className="reviews">
-                    <button className="btn_close transition"
-                        onClick={() => setRevActive(false)}>х</button>
-                    <div className="reviews__product">
-                        <p><img src={product.pictures} alt={product.name} className="reviews__img" /></p>
-                        <div className="reviews__name">{product.name} </div>
+                            <form onSubmit={sendForm} className="form form_auth">
+                                <label>
+                                    <h2>Ваш отзыв</h2>
+                                    <textarea
+                                        placeholder="Введите текст"
+                                        value={text}
+                                        onChange={(e) => setText(e.target.value)}
+
+                                    />
+                                </label>
+                                <button className="modal-link">Отправить</button>
+                            </form>
+                        </div>
                     </div>
-                    <form onSubmit={sendForm}>
-                        <label>
-                            <h3>Ваш отзыв</h3>
-                            <textarea
-                                placeholder="Введите текст"
-                                value={text}
-                                onChange={(e) => setText(e.target.value)}
-                            />
-                        </label>
-                        <button className="modal-link maxwidth_btn">Отправить</button>
-                    </form>
+
                 </div>
             </div>
 
-</Layout> }
+        </Layout>}
     </>
- }
- export {SingleProduct};
+}
+export { SingleProduct };
